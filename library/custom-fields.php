2cba9e941f6f2f71ab0d6c48c45bb36d48638970
Laura Heino <laura@lauraheino.com>
2016-12-21 09:19:47 +0200

    ACF fields for dogs + multiple ownership

---
+++ library/custom-fields.php
@@ -0,0 +1,32 @@
+<?php
+
+function dogium_add_dog_select_friends($field) {
+	global $post;
+	// reset
+	$field['choices'] = array();
+	$data = array();
+
+	// this will fall back to current user if author is not yet defined -LH
+	$author = get_post_field('post_author', $post->ID);
+	$friend_ids = friends_get_friend_user_ids($author);
+	$friend_ids[] = $author;
+	
+	foreach($friend_ids as $friend_id) {
+
+		$user = get_userdata($friend_id);
+		$data[] = array('name' => $user->display_name, 'id' => $friend_id, 'email' => $user->user_email);
+	}
+
+
+	if (is_array($data)) {
+		foreach($data as $key=>$val) {
+			$choice = $val['id'];
+			$field['choices'][$choice] = $val['name'] . ' | ' . $val['email'];
+		}
+	}
+
+	$field['default_value'] = $author;
+	return $field;
+}
+
+add_filter('acf/load_field/name=dgm_owners', 'dogium_add_dog_select_friends');
---
+++ library/custom-post-types.php
@@ -0,0 +1,55 @@
+<?php
+
+// Dog post type
+if ( ! function_exists('dogium_register_dog_post_type') ) {
+	function dogium_register_dog_post_type() {
+		$labels = array(
+			'name' => _x('Dogs', 'post type general name'),
+			'singular_name' => _x('Dog', 'post type singular name'),
+			'add_new' => _x('Add new', 'dog'),
+			'add_new_item' => __('Add new dog', 'dogium'),
+			'edit_item' => __('Edit dog', 'dogium'),
+			'new_item' => __('New dog', 'dogium'),
+			'all_items' => __('All dogs', 'dogium'),
+			'view_item' => __('View dog', 'dogium'),
+			'search_items' => __('Search dogs', 'dogium'),
+			'not_found' => __('No dogs found', 'dogium'),
+			'not_found_in_trash' => __('No dogs found in the trash', 'dogium'),
+			'parent_item_colon' => '&rarr;',
+			'menu_name' => 'Dogs'
+		);
+
+		$args = array(
+			'labels' => $labels,
+			'description' => __('Dogs owned by community members', 'dogium'),
+			'public' => true,
+			'has_archive' => true,
+			'hierarchical' => true,
+			'rewrite' => array('slug' => __('dog', 'dogium')),
+			'supports' => array('title', 'editor', 'author', 'thumbnail', 'comments', 'page-attributes'),
+	 	);
+
+	 	register_post_type('dogium-dog', $args);
+
+	}
+}
+
+// "Breed" custom taxonomy for Dogs
+if (! function_exists('dogium_register_breed_taxonomy')) {
+	function dogium_register_breed_taxonomy() {
+		register_taxonomy(
+			'dogium-breed',
+			'dogium-dog',
+			array(
+				'label' => __('Breed', 'dogium'),
+				'hierarchical' => true,
+				'rewrite' => array(
+					'slug' => __('breed', 'dogium'),
+				)
+			)
+		);
+	}
+}
+
+add_action('init', 'dogium_register_dog_post_type');
+add_action('init', 'dogium_register_breed_taxonomy');
