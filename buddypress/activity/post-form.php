6b813009e48aa436739260f2d54fc8a70146addd
Laura Heino <laura@lauraheino.com>
2017-01-17 13:38:42 +0200

    Remove 'post to group' option from community page

--- buddypress/activity/post-form.php
+++ buddypress/activity/post-form.php
@@ -47,32 +47,7 @@
 				<input type="submit" name="aw-whats-new-submit" id="aw-whats-new-submit" value="<?php esc_attr_e( 'Post Update', 'buddypress' ); ?>" />
 			</div>
 
-			<?php if ( bp_is_active( 'groups' ) && !bp_is_my_profile() && !bp_is_group() ) : ?>
-
-				<div id="whats-new-post-in-box">
-
-					<?php _e( 'Post in', 'buddypress' ); ?>:
-
-					<label for="whats-new-post-in" class="bp-screen-reader-text"><?php
-						/* translators: accessibility text */
-						_e( 'Post in', 'buddypress' );
-					?></label>
-					<select id="whats-new-post-in" name="whats-new-post-in">
-						<option selected="selected" value="0"><?php _e( 'My Profile', 'buddypress' ); ?></option>
-
-						<?php if ( bp_has_groups( 'user_id=' . bp_loggedin_user_id() . '&type=alphabetical&max=100&per_page=100&populate_extras=0&update_meta_cache=0' ) ) :
-							while ( bp_groups() ) : bp_the_group(); ?>
-
-								<option value="<?php bp_group_id(); ?>"><?php bp_group_name(); ?></option>
-
-							<?php endwhile;
-						endif; ?>
-
-					</select>
-				</div>
-				<input type="hidden" id="whats-new-post-object" name="whats-new-post-object" value="groups" />
-
-			<?php elseif ( bp_is_group_activity() ) : ?>
+			<?php if ( bp_is_group_activity() ) : ?>
 
 				<input type="hidden" id="whats-new-post-object" name="whats-new-post-object" value="groups" />
 				<input type="hidden" id="whats-new-post-in" name="whats-new-post-in" value="<?php bp_group_id(); ?>" />
