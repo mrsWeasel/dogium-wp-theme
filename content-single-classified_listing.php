534119ed4986cbd1c57b23075ba86d22488d976e
Laura Heino <laura@lauraheino.com>
2017-01-26 10:18:16 +0200

    Allow reporting inappropriate content for registered users only

--- content-single-classified_listing.php
+++ content-single-classified_listing.php
@@ -31,6 +31,7 @@
 			<a href="#" data-open="flag-as-unappropriate"><i style="color: red" class="fa fa-flag" aria-hidden="true"></i> <?php esc_html_e('Report unappropriate content', 'dogium'); ?></a>
 			<div class="reveal" id="flag-as-unappropriate" data-reveal>
 			<h3><?php esc_html_e('Report unappropriate content', 'dogium'); ?>: <?php echo get_the_title(); ?></h3>
+			<?php if (is_user_logged_in()) : ?>
 		    <p><?php esc_html_e('Why do you think this item is unappropriate?', 'dogium'); ?></p>
 			<form action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="post">
 				<label for="flag-message"><?php esc_html_e('Message (required)', 'dogium'); ?></label>
@@ -42,7 +43,20 @@
 				<input type="hidden" name="action" value="flag_unappropriate">
 				<input type="hidden" name="post_id" value="<?php echo esc_attr($post->ID); ?>">
 			</form>
+			<?php else : ?>
+				<?php $login_url = wp_login_url( get_permalink() ); ?>
+				<?php $register_url = wp_registration_url(); ?>
 
+				<div class="callout warning">
+				<p>
+				<?php echo sprintf(
+				__('This action is for logged in users only. Please <a href="%s">login</a> or <a href="%s">register</a>.', 'dogium'), 
+				$login_url,
+				$register_url
+				); ?>
+				</p>
+				</div>
+			<?php endif; ?>
 			<button class="close-button" data-close aria-label="<?php esc_attr_e('Close modal', 'dogium');?>" type="button">
 			    <span aria-hidden="true">&times;</span>
 			</button>
