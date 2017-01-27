e24091f3a451797ac4e2859ba6f5f2b0bc4b57d9
Laura Heino <laura@lauraheino.com>
2017-01-27 12:32:00 +0200

    Edit entry meta content and design

--- library/entry-meta.php
+++ library/entry-meta.php
@@ -8,7 +8,9 @@
 
 if ( ! function_exists( 'foundationpress_entry_meta' ) ) :
 	function foundationpress_entry_meta() {
-		echo '<time class="updated" datetime="' . get_the_time( 'c' ) . '">' . sprintf( __( 'Posted on %1$s at %2$s.', 'foundationpress' ), get_the_date(), get_the_time() ) . '</time>';
-		echo '<p class="byline author">' . __( 'Written by', 'foundationpress' ) . ' <a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" rel="author" class="fn">' . get_the_author() . '</a></p>';
+		global $post;
+		$author_id = $post->post_author;
+		echo '<time class="updated" datetime="' . get_the_time( 'c' ) . '">' . '<i class="fa fa-calendar" aria-hidden="true"></i> ' . get_the_date() . '</time>';
+		echo '<span class="byline author">' . '<i class="fa fa-user" aria-hidden="true"></i> ' . get_the_author_meta('display_name', $author_id) . '</span>';
 	}
 endif;
