09c02b6d0b11815ef7e156e7c04bcbffd008bdd5
Laura Heino <laura@lauraheino.com>
2017-01-27 12:29:06 +0200

    Home / archive post loop apperance

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,17 +9,25 @@
  */
 
 ?>
-
-<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
-	<header>
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		<?php foundationpress_entry_meta(); ?>
-	</header>
-	<div class="entry-content">
-		<?php the_content( __( 'Continue reading...', 'foundationpress' ) ); ?>
+<?php if (is_home() || is_archive()) : ?>
+	<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry large-4 columns end'); ?>>
+		<div class="card">
+			<?php if (has_post_thumbnail()) : ?>
+				<div class="thumbnail">
+					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('featured-small'); ?></a>
+				</div>	
+			<?php endif; ?>
+			<div class="card-section">
+				<header>
+					<h2 class="secondary-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				</header>
+				<div class="entry-content">
+					
+				</div>
+				<footer>
+					<?php $tag = get_the_tags(); if ( $tag ) { ?><p class="tags"><?php the_tags('', ' ', ''); ?></p><?php } ?>
+				</footer>
+			</div>	
+		</div>
 	</div>
-	<footer>
-		<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
-	</footer>
-	<hr />
-</div>
+<?php endif;
