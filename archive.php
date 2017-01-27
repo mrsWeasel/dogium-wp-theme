acf64c49a1337481ec9d66bb5d75bc3966e9545b
Laura Heino <laura@lauraheino.com>
2017-01-27 12:26:20 +0200

    Archive page template (header + loop mods)

--- archive.php
+++ archive.php
@@ -1,31 +1,70 @@
 <?php
 /**
- * The template for displaying archive pages
- *
- * Used to display archive-type pages if nothing more specific matches a query.
- * For example, puts together date-based pages if no date.php file exists.
- *
- * If you'd like to further customize these archive views, you may create a
- * new template file for each one. For example, tag.php (Tag archives),
- * category.php (Category archives), author.php (Author archives), etc.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * The template for blog archives
  *
  * @package FoundationPress
  * @since FoundationPress 1.0.0
  */
 
-get_header(); ?>
+get_header();
+
+// We use main blog page header image for our archives, too
+$id = get_option( 'page_for_posts' );
+$dgm_background = get_field('dgm_background', $id);
+
+$page_title = $page_subtitle = '';
+	// What kind of archive is it?
+	if ( is_category() ) :
+		$category = get_the_category($post->ID);
+		$page_title = $category[0]->name;
+		$page_subtitle = sprintf(esc_html('All posts in category "%s"', 'dogium'), $page_title);
+	elseif ( is_tag() ) :
+		$page_title = single_tag_title('', false);
+		$page_subtitle = sprintf(esc_html('All posts tagged "%s"', 'dogium'), $page_title);
+	elseif ( is_author() ) :
+		$author = get_userdata( get_query_var('author') );
+		$page_title = $author->nickname;
+		$page_subtitle = sprintf(esc_html('All posts written by %s', 'dogium'), $page_title);
+	elseif (is_day()) :
+		$page_title = get_the_date();
+		$page_subtitle = esc_html('Daily archive', 'dogium'); 
+	elseif (is_month()) :
+		$page_title = get_the_date('F Y');
+		$page_subtitle = esc_html('Monthly archive', 'dogium'); 
+	elseif (is_year()) :
+		$page_title = get_the_date('Y');
+		$page_subtitle = esc_html('Yearly archive', 'dogium'); 
+	endif; 	
+?>
+<?php if ('' != $dgm_background) : ?>
+<header id="page-header" style="background-image: url(<?php echo esc_url( $dgm_background ); ?>);">
+<?php else : ?>
+<header id="page-header">  
+<?php endif; ?>
+    <div class="row">
+        <div class="medium-8 medium-centered columns">    
+            <h1 class="text-center white text-shadow entry-title"><?php echo $page_title; ?></h1>
+            <p class="intro text-center white text-shadow"><?php echo $page_subtitle;?></p>
+        </div>
+    </div>
+</header> 
 
 <div id="page" role="main">
 	<article class="main-content">
 	<?php if ( have_posts() ) : ?>
 
 		<?php /* Start the Loop */ ?>
+		<?php $counter = 0; ?>
+		<div class="row">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-		<?php endwhile; ?>
 
+			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+			<?php if ( ++$counter % 3 === 0 ) : ?>
+				</div>
+				<div class="row">
+			<?php endif; ?>	
+		<?php endwhile; ?>
+		</div>
 		<?php else : ?>
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
