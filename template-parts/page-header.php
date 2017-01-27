ef4f27811ebde4f7e4619205d5c22f9e184c21fe
Laura Heino <laura@lauraheino.com>
2017-01-27 12:28:39 +0200

    Page header conditional statements + cleanup

--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -1,18 +1,24 @@
   <?php
-  $dgm_background = get_field('dgm_background');
-  if ( '' != $dgm_background ) {
-      $bg_url = esc_url( $dgm_background );
+  $id = '';
+
+  // If we don't do this for news page (home), WP is going to get id from the first post in loop
+  if ( is_home() ) {
+    $id = get_option( 'page_for_posts' );
   } else {
-      $bg_url = '';
-  } 
+    $id = $post->ID;
+  }
 
+  $dgm_background = get_field('dgm_background', $id);
 
-  $page_title = get_post_meta($post->ID, 'dgm_custom_heading', true);
-  if ('' == $page_title) {
+  $page_title = get_post_meta($id, 'dgm_custom_heading', true);
+  if ( '' == $page_title && is_home() ) {
+    $page_title = esc_html('News', 'dogium');
+  } elseif ('' == $page_title) {
     $page_title = get_the_title();
   }
-  $page_subtitle = get_post_meta($post->ID, 'dgm_subheading', true);
+  $page_subtitle = get_post_meta($id, 'dgm_subheading', true);
 
+  // bbPress forum header
   if ( function_exists('bbp_get_forum_archive_title') && function_exists( 'bbp_get_topic_archive_title' )) {
     if ( bbp_is_forum_archive() ) {
       $page_title = bbp_get_forum_archive_title();
@@ -22,15 +28,17 @@
   }
 
   ?>
-<?php if ('' != $bg_url) : ?>
-<header id="page-header" style="background-image: url(<?php echo $bg_url; ?>);">
+<?php if ('' != $dgm_background) : ?>
+<header id="page-header" style="background-image: url(<?php echo esc_url( $dgm_background ); ?>);">
 <?php else : ?>
 <header id="page-header">  
 <?php endif; ?>
     <div class="row">
         <div class="medium-8 medium-centered columns">    
             <h1 class="text-center white text-shadow entry-title"><?php echo esc_html($page_title); ?></h1>
-            <?php if ('' != $page_subtitle) : ?>
+            <?php if ( is_singular('post') ) : ?>
+              <div class="header-entry-meta"><?php foundationpress_entry_meta(); ?></div>
+            <?php elseif ('' != $page_subtitle) : ?>
             <p class="intro text-center white text-shadow"><?php echo esc_html($page_subtitle);?></p>
             <?php endif; ?> 
         </div>
