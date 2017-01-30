0f20a43602de95c0a0873146d267c52302cd1bc7
Laura Heino <laura@lauraheino.com>
2017-01-30 15:38:22 +0200

    Add version number from wp get theme, remove enqueueing unnecessary styles /scripts

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -12,25 +12,21 @@
 if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	function foundationpress_scripts() {
 
+	$theme = wp_get_theme();
+	$version = $theme->get('Version');	
+
 	// Enqueue the main Stylesheet.
-	wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/assets/stylesheets/foundation.css', array(), '2.6.1', 'all' );
+	wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/assets/stylesheets/foundation.css', array(), $version, 'all' );
 	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700');
 	// If you'd like to cherry-pick the foundation components you need in your project, head over to gulpfile.js and see lines 35-54.
 	// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
-	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), '2.6.1', true );
+	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), $version, true );
 
 	// Add the comment-reply library on pages where it is necessary
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	// We're only using gallery slider on single dog view
-	if ( is_singular('dogium_dog') ) {
-		wp_enqueue_style('featherlight', get_template_directory_uri() . '/assets/stylesheets/featherlight/featherlight.css');
-		wp_enqueue_style('featherlight', get_template_directory_uri() . '/assets/stylesheets/featherlight/featherlight.gallery.css', array('featherlight'));
-		wp_enqueue_script('featherlight', get_template_directory_uri() . '/assets/javascript/featherlight/featherlight.js', array('jquery'));
-		wp_enqueue_script('featherlight-gallery', get_template_directory_uri() . '/assets/javascript/featherlight/featherlight.gallery.js', array('featherlight'));
-	}
 
 }
 
