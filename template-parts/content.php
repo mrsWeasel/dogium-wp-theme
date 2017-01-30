a8871911eaacaa71b9c3a65fdd896f445915fe0e
Laura Heino <laura@lauraheino.com>
2017-01-30 15:33:21 +0200

    Modify home / archive post loop appearance

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,26 +10,28 @@
 
 ?>
 <?php if (is_home() || is_archive()) : ?>
-	<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry large-4 columns end'); ?>>
-		<div class="card">
-			<?php if (has_post_thumbnail()) : ?>
-				<div class="thumbnail">
-					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('featured-small'); ?></a>
-				</div>	
-			<?php endif; ?>
-			<div class="card-section">
-				<header>
-					<h2 class="secondary-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				</header>
-				<div class="entry-content">
-					
-				</div>
-				<footer>
-					<?php $tag = get_the_tags(); if ( $tag ) { ?><p class="tags"><?php the_tags('', ' ', ''); ?></p><?php } ?>
-				</footer>
-			</div>	
+	
+		<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry large-4 columns end dogium-excerpt'); ?>>
+			<div class="card">
+				<a href="<?php the_permalink(); ?>">
+					<?php if (has_post_thumbnail()) : ?>
+					<div class="thumbnail">
+						<?php the_post_thumbnail('featured-small'); ?>
+					</div>	
+					<?php endif; ?>
+					<div class="card-section">
+						<div class="mask"></div>
+						<header>
+							<h2 class="secondary-title"><?php the_title(); ?></h2>
+						</header>
+						<div class="entry-content">
+							<?php the_excerpt(); ?>
+						</div>
+					</div>
+				</a>
+			</div>
 		</div>
-	</div>
+	
 <?php else : ?>
 	<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
 		<header>
