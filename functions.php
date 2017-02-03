aa17f659afabfc69ab685fdcbad33386e24802f9
Laura Heino <laura@lauraheino.com>
2017-02-03 11:08:52 +0200

    Clean up functions.php and move restriction functions to their own file

--- functions.php
+++ functions.php
@@ -74,58 +74,8 @@ require_once( 'library/events-template-tags.php' );
 /** Register event custom fields **/
 require_once( 'library/event-custom-fields.php');
 
-
-function dogium_doing_admin_post() {
-	/**
-	  * @param array | $output | Initialize empty output array for our regex pattern
-	  * @param string | $requerst_uri
-	  * @return bool | True if request made to admin-post.php, false otherwise
-	  */
-	$output = array();
-	$request_uri = $_SERVER['REQUEST_URI'];
-	preg_match( "/[\/\-a-zA-Z0-0]*wp-admin\/admin-post.php(\/)?$/", $request_uri, $output );
-	if (!empty($output) && $output[0] !== '') {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-add_action( 'init', 'blockusers_init' );
-function blockusers_init() {
-	// 
-	if ( is_admin() && ! current_user_can( 'manage_options' ) && ! wp_doing_ajax() && ! dogium_doing_admin_post() ) {
-		wp_redirect( home_url() );
-		exit;
-	}
-}
-
-add_filter( 'ajax_query_attachments_args', 'show_users_own_attachments', 1, 1 );
-function show_users_own_attachments( $query ) 
-{
- $id = get_current_user_id();
- if( !current_user_can('manage_options') )
- $query['author'] = $id;
- return $query;
-}
-
-add_action('after_setup_theme', 'remove_admin_bar');
-
-function remove_admin_bar() {
-	if (!current_user_can('manage_options') && !is_admin()) {
-	  show_admin_bar(false);
-	}
-}
-
-// Testing: remove medialibrary tab
-function remove_medialibrary_tab($tabs) {
-    if ( !current_user_can( 'level_5' ) ) {
-        unset($tabs['library']);
-    }
-    return $tabs;
-}
-add_filter('media_upload_tabs', 'remove_medialibrary_tab');
-//add_filter( 'pre_get_posts' , 'show_more_posts' );
+/** Add some restrictions to basic users **/
+require_once( 'library/restrictions.php');
 
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/protocol-relative-theme-assets.php' );
---
+++ library/restrictions.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package Dogium
+ * @since 1.0.2
+ * @author Laura Heino
+ */
+
+function dogium_doing_admin_post() {
+	/**
+	  * @param array | $output | Initialize empty output array for our regex pattern
+	  * @param string | $requerst_uri
+	  * @return bool | True if request made to admin-post.php, false otherwise
+	  */
+	$output = array();
+	$request_uri = $_SERVER['REQUEST_URI'];
+	preg_match( "/[\/\-a-zA-Z0-0]*wp-admin\/admin-post.php(\/)?$/", $request_uri, $output );
+	if (!empty($output) && $output[0] !== '') {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+add_action( 'init', 'dogium_block_users_from_admin' );
+
+function dogium_block_users_from_admin() {
+	// Redirect basic users from admin area
+	// wp_doing_ajax() works from version 4.7 - we should not need support for legacy WP versions.
+	if ( is_admin() && ! current_user_can( 'manage_options' ) && ! wp_doing_ajax() && ! dogium_doing_admin_post() ) {
+		wp_redirect( home_url() );
+		exit;
+	}
+}
+
+
+add_filter( 'ajax_query_attachments_args', 'dogium_show_users_own_attachments', 1, 1 );
+
+function dogium_show_users_own_attachments( $query ) {
+	// Only allow viewing OWN items in medialibrary
+	 $id = get_current_user_id();
+	 if( !current_user_can('manage_options') )
+	 $query['author'] = $id;
+	 return $query;
+}
+
+add_action('after_setup_theme', 'dogium_remove_admin_bar');
+
+function dogium_remove_admin_bar() {
+	if (!current_user_can('manage_options') && !is_admin()) {
+	  show_admin_bar(false);
+	}
+}
