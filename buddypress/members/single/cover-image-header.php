dbe448b953d69fadfec2139c5b260bade28cdbb5
Laura Heino <laura@lauraheino.com>
2017-02-06 15:38:16 +0200

    Profile page design

--- buddypress/members/single/cover-image-header.php
+++ buddypress/members/single/cover-image-header.php
@@ -23,27 +23,32 @@ do_action( 'bp_before_member_header' ); ?>
           'item_id' => bp_displayed_user_id(),
         ));
 	$change_cover_link = bp_loggedin_user_domain() . 'profile/change-cover-image/';
+	$profile_link = bp_loggedin_user_domain() . 'profile/';
+	$settings_link = bp_loggedin_user_domain() . 'settings/';
 	?>
 <div id="dogium-member-cover" class="clearfix" style="background-image: url(<?php echo esc_url($cover_image_url);?>)">
 	<?php
 	if ( bp_displayed_user_id() === get_current_user_id() ) : ?>
 	<a id="change-cover-link" title="<?php esc_attr_e('Change cover image', 'dogium');?>" href="<?php echo esc_url( $change_cover_link );?>"><span class="screen-reader-text"><?php esc_html_e('Change cover image', 'dogium');?></span><i class="fa fa-photo" aria-hidden="true"></i></a>
-
+	<div id="profile-settings-container">
+		<a id="user-profile-link" title="<?php esc_attr_e('Edit profile fields', 'dogium'); ?>" href="<?php echo esc_url( $profile_link ); ?>"><span class="screen-reader-text"><?php esc_html_e('Edit profile fields', 'dogium');?></span><i class="fa fa-user" aria-hidden="true"></i></a>
+		<a id="privacy-settings-link" title="<?php esc_attr_e('Privacy / login settings', 'dogium'); ?>" href="<?php echo esc_url( $settings_link ); ?>"><span class="screen-reader-text"><?php esc_html_e('Edit profile fields', 'dogium');?></span><i class="fa fa-gear" aria-hidden="true"></i></a>
+	</div>
 	<?php endif; ?>
 	<div class="row">
 		<div class="item-header-container-wrap medium-10 columns medium-centered">
 			<div id="item-header-container">
-			<div id="item-header-avatar">
-				<a href="<?php bp_displayed_user_link(); ?>">
+				<div id="item-header-avatar">
+					<a href="<?php bp_displayed_user_link(); ?>">
 
-					<?php bp_displayed_user_avatar( 'type=full' ); ?>
+						<?php bp_displayed_user_avatar( 'type=full' ); ?>
 
-				</a>
-			</div><!-- #item-header-avatar -->
+					</a>
+				</div><!-- #item-header-avatar -->
 			<div id="item-header-content">
 
 				<?php if ( bp_is_active( 'activity' ) && bp_activity_do_mentions() ) : ?>
-					<h1 class="white text-shadow"><?php echo bp_core_get_user_displayname( bp_displayed_user_id() ); ?>
+					<h1 class="white text-shadow"><?php echo bp_core_get_user_displayname( bp_displayed_user_id() ); ?><br>
 					<small>@<?php bp_displayed_user_mentionname(); ?></small></h1>
 
 				<?php endif; ?>
