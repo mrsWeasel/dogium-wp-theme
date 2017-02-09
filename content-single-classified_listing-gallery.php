460e6386549f6fb3cc794e13612b58b8616c0e08
Laura Heino <laura@lauraheino.com>
2017-02-09 16:07:23 +0200

    Style classifieds images

--- content-single-classified_listing-gallery.php
+++ content-single-classified_listing-gallery.php
@@ -1,14 +1,16 @@
 <?php do_action( 'single_classified_listing_gallery_start' ); ?>
 
-<div class="classified_images" itemscope itemtype="http://data-vocabulary.org/Organization">
+<div class="classified_images" id="featured-classified-image" itemscope itemtype="http://data-vocabulary.org/Organization">
 
 	<!-- Customized image size (default would be full size) LH -->
-	<?php the_classified_featured_image('featured-small'); ?>
 
+	<?php the_classified_featured_image('fp-medium-height', false); ?>
+	<?php global $post; ?>
+	<?php dogium_the_classified_price('<span class="label primary main-price">', '</span>', $post); ?>
 </div>
 
 <div class="classified_images_gallery" itemscope itemtype="http://data-vocabulary.org/Organization">
-	<?php the_classified_images(0, 'featured-xsmall'); ?>
+	<?php the_classified_images(0, 'fp-small-height'); ?>
 </div>
 
 <?php do_action( 'single_classified_listing_gallery_end' ); ?>
