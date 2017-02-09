c092a884abe20969b4c4f62848162ff83100a893
Laura Heino <laura@lauraheino.com>
2017-02-09 16:05:45 +0200

    Remove price from classifieds sidebar

--- sidebar-classified_listing.php
+++ sidebar-classified_listing.php
@@ -1,10 +1,6 @@
 <?php if ( is_active_sidebar( 'classified-manager-sidebar-single' )  ) : ?>
 
 	<aside id="classified-manager-single-sidebar-secondary" role="complementary">
-		<!-- Our own price formatting function (append € instead of prepending) LH -->
-		<?php dogium_the_classified_price('<div class="main-price classified-price">', '</div>'); ?>
-
-
 		<?php if ( users_can_contact() ) : ?>
 		<?php get_classified_manager_template( 'classified-contact.php' ); ?>
 		<?php endif; ?>
