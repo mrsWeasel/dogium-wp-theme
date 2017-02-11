138dd345c4532a9958b26a2bc89a308f013032bb
Laura Heino <laura@lauraheino.com>
2017-02-11 22:28:58 +0200

    Fix problem with global EM_Event

--- single-event.php
+++ single-event.php
@@ -5,12 +5,20 @@
  * @package Dogium
  * @since Dogium 1.0.0
  */
+global $post;
+$EM_Event = em_get_event($post->ID, 'post_id');
 
 get_header();
-get_template_part( 'template-parts/page-header-thin' );
+get_template_part( 'template-parts/page-header-thin' ); ?>
 
+
+<div id="page-full-width" role="main">
+
+<?php do_action( 'foundationpress_before_content' ); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+
+<?php
 // We need to treat different event categories
-global $post;
 $event_category = '';
 $event_categories = get_the_terms($post->ID, 'event-categories');
 
@@ -26,16 +34,21 @@ if ($event_categories) {
 	}
 }
 
-?>
-<div id="page-full-width" role="main">
+$group_id = $EM_Event->group_id;
+$user = get_current_user_id();	
+$group = groups_get_group( array( 'group_id' => $group_id ) );
+$is_member = groups_is_user_member($user, $group_id); ?>
 
-<?php do_action( 'foundationpress_before_content' ); ?>
-<?php while ( have_posts() ) : the_post(); ?>
 	<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-		<header>
-			<h1 class="entry-title blue"><?php the_title(); ?></h1>
-			<?php echo do_shortcode("[events_list post_id='{$post->ID}']#_EVENTCATEGORIES[/events_list]");?>
-		</header>
+		<?php if ( $slug == 'ryhmatapahtuma' && ( $group == '' || ( $group->status !== 'public' && ! $is_member ) ) ) : ?>
+			<div class="callout warning"><p><?php esc_html_e('This content is private.', 'dogium'); ?></p></div>
+		<?php else : ?>	
+			<header>
+				<h1 class="entry-title blue"><?php the_title(); ?></h1>
+
+				<?php echo do_shortcode("[events_list post_id='{$post->ID}']#_EVENTCATEGORIES[/events_list]");?>
+			</header>
+		<?php endif; ?>
 		<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
 		<div class="entry-content">
 			<?php get_template_part('template-parts/content-events', $slug); ?>
--- template-parts/content-events-ryhmatapahtuma.php
+++ template-parts/content-events-ryhmatapahtuma.php
@@ -1,34 +1,70 @@
+<?php
+global $post;
+$EM_Event = em_get_event($post->ID, 'post_id');
+?>
 <div class="row collapse">
 	<div class="large-6 columns">
-	<div class="event-details">
-	<?php echo do_shortcode("[events_list post_id='{$post->ID}']#_EVENTIMAGE{900,900}[/events_list]"); ?>
+	
+	<div class="event-details">	
+	<?php echo do_shortcode("[events_list post_id='{$post->ID}']#_EVENTIMAGE{150,150}[/events_list]"); ?>
 	</div>
 	<div class="event-details">
 	<?php echo do_shortcode("[events_list post_id='{$post->ID}']#_EVENTNOTES[/events_list]"); ?>
 	<strong><?php esc_html_e('Date(s)', 'dogium'); ?></strong>: <?php echo do_shortcode("[events_list post_id='{$post->ID}']#_{j.n.Y} - #@_{j.n.Y}[/events_list]");?><br>
+
+	<?php if ($EM_Event->has_location) : ?>
 	<strong><?php esc_html_e('Venue', 'dogium'); ?></strong>: <?php echo do_shortcode("[events_list post_id='{$post->ID}']#_LOCATIONNAME[/events_list]"); ?><br>
 	<strong><?php esc_html_e('Contact information', 'dogium'); ?></strong>: <br>
 	<?php echo do_shortcode("[events_list post_id='{$post->ID}']#_LOCATIONADDRESS[/events_list]"); ?><br>
 	<?php echo do_shortcode("[events_list post_id='{$post->ID}']#_LOCATIONPOSTCODE[/events_list]"); ?> <?php echo do_shortcode("[events_list post_id='{$post->ID}']#_LOCATIONTOWN[/events_list]"); ?><br>
+	<?php endif; ?>
 	</div>
 	<div class="event-details">
 	<h2 class="secondary-title"><?php esc_html_e('Attendees', 'dogium');?></h2>
 	<?php echo do_shortcode("[events_list post_id='{$post->ID}']#_ATTENDEES[/events_list]");?>
+
+	<?php if ($EM_Event->has_bookings) : ?>
 	<strong><?php esc_html_e('Available spaces:', 'dogium'); ?></strong> <?php echo do_shortcode("[events_list post_id='{$post->ID}']#_AVAILABLESPACES[/events_list]"); ?>
+	 <?php endif; ?>
+
 	</div>
 	<div class="event-details">
-		<?php echo do_shortcode("[events_list post_id='{$post->ID}']#_BOOKINGBUTTON[/events_list]");?>
+		<?php
+		/**
+	     * We need this for our custom privileges (only group members can attend
+	     * if group is private / closed). All logged in users can attend if group is public
+	     * @since Dogium 1.0
+	     * @author Laura Heino
+	     */
+	    $user = get_current_user_id();
+	    // Get group with ownership of this event
+	    $group_id = $EM_Event->group_id;
+	    $group = groups_get_group( array( 'group_id' => $group_id ) );
+		$is_member = groups_is_user_member($user, $group_id);
+		// Initialize
+		$user_can_book = false;
+
+		$group_visibility = $group->status;
+		switch ($group_visibility) {
+			case 'hidden':
+				$user_can_book = $is_member ? true : false;
+				break;
+			case 'private':
+				$user_can_book = $is_member ? true : false;
+				break;
+			default:
+				$user_can_book = is_user_logged_in() ? true : false;
+				break;
+		}
+
+	if ($EM_Event->has_bookings && $user_can_book) {	
+	echo do_shortcode("[events_list post_id='{$post->ID}']#_BOOKINGBUTTON[/events_list]");
+	} ?>
 	</div>
 	</div>
 	<div class="large-5 columns">
 		
 		<?php echo do_shortcode("[events_list post_id='{$post->ID}']#_LOCATIONMAP[/events_list]");?>
-		<?php global $EM_Event; ?>
-		Laura muista poistaa nämä
-		<p>Omistaja (kirjoittaja)</p>
-		<?php var_dump($EM_Event->event_owner); ?>
-		<p>Ryhmä</p>
-		<?php var_dump($EM_Event->group_id); ?>
-	</div>
 
+	</div>
 </div>
