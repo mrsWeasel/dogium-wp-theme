e891522e6364fc27b74e4ac6de3592b5e57626df
Laura Heino <laura@lauraheino.com>
2017-02-13 20:59:12 +0200

    Pre get posts (events)

---
+++ library/events-manager-mods.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package Dogium
+ * @since 1.0
+ * @author Laura Heino
+ */
+
+function dogium_show_more_events( $query ) {
+	if (is_admin() || ! $query->is_main_query() ) {
+		return;
+	}
+	if (is_taxonomy('event-categories')) {
+		$query->set('posts_per_page', 50);
+		return;
+	}
+}
+
+add_action('pre_get_posts', 'dogium_show_more_events');
