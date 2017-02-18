69b7a0bdb5db7f8f2c5dbc0a0512cdafb9d6956b
Laura Heino <laura@lauraheino.com>
2017-02-18 21:59:14 +0200

    Fix broken gettext

--- searchform.php
+++ searchform.php
@@ -10,7 +10,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php esc_html_e('Search for:', 'dogium'); ?></span>
-		<input type="search" class="search-field" placeholder="<?php esc_attr_e('Search', 'tuomas'); ?>" value="" name="s" />
+		<input type="search" class="search-field" placeholder="<?php esc_attr_e('Search', 'dogium'); ?>" value="" name="s" />
 	</label>
 	<button type="submit" class="search-submit"><i class="fa fa-search" aria-hidden="true"></i><span class="screen-reader-text"><?php esc_html_e('Submit search', 'dogium'); ?></span></button>
 </form>
